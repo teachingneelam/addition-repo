3820843f1d2182319ae34ae5c96d9513c6763afa
nee1 <nee@gmail.com>
2024-07-25 10:12:14 +0530

    multiplication

---
+++ multiplication code/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplication_code
+{
+    internal class Program
+    {
+        int num1;
+        int num2;
+        int result;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first number");
+            int num1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second number");
+            int num2=int.Parse(Console.ReadLine());
+            Console.WriteLine("enter your result is : {0}",num1*num2 );
+            Console.ReadLine();
+        }
+    }
+}
